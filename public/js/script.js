84261bcd9a4ef63a8d6b2ebe87d79f4c8369ffad
Kesh Pratap Singh <kpsingh0152@gmail.com>
2024-09-08 17:45:01 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,10 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+const fileInput = document.getElementById('file-input');
 
-// Get username and room from URl
-const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true,});
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true});
 const socket = io();
 
 // Join chatroom
@@ -17,15 +18,21 @@ socket.on('roomUsers', function({ room, users }){
 });
 
 // Message from server
-socket.on('message', function( message ){
-
+socket.on('message', function(message){
    outputMessage(message);
    // scroll down
    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
+// File message from server
+socket.on('fileMessage', function(fileMessage){
+   outputFileMessage(fileMessage);
+   // scroll down
+   chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
 // Message submit
-chatForm.addEventListener('submit', function( e ){
+chatForm.addEventListener('submit', function(e){
    e.preventDefault();
 
    const msg = e.target.elements.msg.value;
@@ -36,22 +43,71 @@ chatForm.addEventListener('submit', function( e ){
    e.target.elements.msg.focus();
 });
 
+// File input change
+fileInput.addEventListener('change', function(e){
+   const file = e.target.files[0];
+   if (!file) return;
+
+   const formData = new FormData();
+   formData.append('file', file);
+
+   fetch('/upload', {
+      method: 'POST',
+      body: formData
+   })
+   .then(response => response.json())
+   .then(data => {
+      socket.emit('fileMessage', { filename: data.filename, originalname: data.originalname });
+   })
+   .catch(error => console.error('Error:', error));
+
+   // Clear the file input
+   e.target.value = '';
+});
+
 // Output msg to DOM
 function outputMessage(message) {
    const div = document.createElement('div');
    div.classList.add('message');
 
-   div.innerHTML = ` <p class="meta">${message.username} <span>${message.time}</span></p><p class="text">${message.text} </p>`;
+   // Check if the message is from the current user
+   if (message.username === username) {
+      div.classList.add('outgoing');
+   }
+
+   div.innerHTML = `
+      <p class="meta">${message.username === username ? 'You' : message.username} <span>${message.time}</span></p>
+      <p class="text">${message.text}</p>
+   `;
+
+   document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Output file message to DOM
+function outputFileMessage(fileMessage) {
+   const div = document.createElement('div');
+   div.classList.add('message');
+
+   // Check if the message is from the current user
+   if (fileMessage.username === username) {
+      div.classList.add('outgoing');
+   }
+
+   div.innerHTML = `
+      <p class="meta">${fileMessage.username === username ? 'You' : fileMessage.username} <span>${fileMessage.time}</span></p>
+      <p class="text">${fileMessage.text}</p>
+      <p><a href="${fileMessage.fileUrl}" target="_blank" download="${fileMessage.fileName}">Download ${fileMessage.fileName}</a></p>
+   `;
 
    document.querySelector('.chat-messages').appendChild(div);
 }
 
 // Add room name to DOM
 function outputRoomName(room) {
-   roomName.innerHTML = room;
+   roomName.innerText = room;
 }
 
 // Add users to DOM
 function outputUsers(users) {
-   userList.innerHTML = `${users.map((user) => `<li>${user.username}</li>`).join('')}`;
+   userList.innerHTML = users.map(user => `<li>${user.username}</li>`).join('');
 }
